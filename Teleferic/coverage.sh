9f720a8ed4d1a899d55512d6d8ac3a0551b78f7a
Nacho Althabe <nacho.althabe@gmail.com>
2017-09-12 22:24:20 -0300

    Update

--- Teleferic/coverage.sh
+++ Teleferic/coverage.sh
@@ -1,4 +1,18 @@
-#!/bin/bash
-rm htmlcov -Rf
-coverage run --source='.' manage.py test
+#/bin/bash
+
+if [ ! -f ./.env ]
+then
+  virtualenv -p python3 .env
+fi
+
+. .env/bin/activate
+pip install -r requirements.txt
+
+if [ ! -f ./htmlcov ]
+then
+  rm htmlcov -Rf
+fi
+
+DEBUG=1 coverage run --source='.' manage.py test
+
 coverage html 
