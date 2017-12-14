f1b0238e2d43a1c2f8b01397a23b257db1d4bc9f
Nacho Althabe <nacho.althabe@gmail.com>
2017-12-14 17:47:12 -0300

    Ups

--- update.sh
+++ update.sh
@@ -2,12 +2,12 @@
 
 echo "##teamcity[blockOpened name='update_gateway']"
 docker service update himalaya_dev_gateway --with-registry-auth --update-order start-first --detach=false
-echo "##teamcity[blockOpened name='update_gateway']"
+echo "##teamcity[blockClosed name='update_gateway']"
 
 echo "##teamcity[blockOpened name='update_teleferic']"
 docker service update himalaya_dev_teleferic --with-registry-auth --update-order start-first --detach=false
-echo "##teamcity[blockOpened name='update_teleferic']"
+echo "##teamcity[blockClosed name='update_teleferic']"
 
 echo "##teamcity[blockOpened name='update_pgdb']"
 docker service update himalaya_dev_pgdb --with-registry-auth --update-order start-first --detach=false
-echo "##teamcity[blockOpened name='update_pgdb']"
+echo "##teamcity[blockClosed name='update_gateway']"
