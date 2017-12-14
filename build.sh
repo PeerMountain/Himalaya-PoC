7b80ca283821c7de61c5c5aed4ff6c85482a5599
Nacho Althabe <nacho.althabe@gmail.com>
2017-12-14 13:51:43 -0300

    Adding log info

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+echo "Build Images"
 docker-compose build
-
+echo "Push gateway"
 docker push meteoro.dxmarkets.com:5000/himalaya_gateway:latest
-
+echo "Push teleferic"
 docker push meteoro.dxmarkets.com:5000/himalaya_teleferic:latest
