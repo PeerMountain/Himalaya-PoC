11769369c75732e53e863f949e48797622b63b9b
Nacho Althabe <nacho.althabe@gmail.com>
2017-12-14 17:39:29 -0300

    Update swarm

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "##teamcity[compilationStarted compiler='image_builder']"
-docker-compose -f docker-compose.prod.yml build
-echo "##teamcity[compilationFinished compiler='image_builder']"
-
-echo "##teamcity[blockOpened name='image_push']"
-docker-compose -f docker-compose.prod.yml push
-echo "##teamcity[blockClosed name='image_push']"
---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "##teamcity[blockOpened name='update_gateway']"
+docker service update himalaya_dev_gateway --with-registry-auth --update-order start-first --detach=false
+echo "##teamcity[blockOpened name='update_gateway']"
+
+echo "##teamcity[blockOpened name='update_teleferic']"
+docker service update himalaya_dev_teleferic --with-registry-auth --update-order start-first --detach=false
+echo "##teamcity[blockOpened name='update_teleferic']"
+
+echo "##teamcity[blockOpened name='update_pgdb']"
+docker service update himalaya_dev_pgdb --with-registry-auth --update-order start-first --detach=false
+echo "##teamcity[blockOpened name='update_pgdb']"
