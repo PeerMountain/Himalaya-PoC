88d600f5c63d308cc2b049feb6887e958dbb0aa8
Nacho Althabe <nacho.althabe@gmail.com>
2017-12-14 14:53:18 -0300

    Mistyping on docker compose version Bad reference to docker compose file on build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 
 echo "##teamcity[compilationStarted compiler='image_builder']"
-docker-compose build
+docker-compose -f docker-compose.prod.yml build
 echo "##teamcity[compilationFinished compiler='image_builder']"
 
-echo "##teamcity[blockOpened name='push-himalaya_gateway']"
-docker push meteoro.dxmarkets.com:5000/himalaya_gateway:latest
-echo "##teamcity[blockClosed name='push-himalaya_gateway']"
-
-echo "##teamcity[blockOpened name='push-himalaya_teleferic']"
-docker push meteoro.dxmarkets.com:5000/himalaya_teleferic:latest
-echo "##teamcity[blockClosed name='push-himalaya_teleferic']"
+echo "##teamcity[blockOpened name='image_push']"
+docker-compose -f docker-compose.prod.yml push
+echo "##teamcity[blockClosed name='image_push']"
