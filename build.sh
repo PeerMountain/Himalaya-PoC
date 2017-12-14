9432f6bd1c97394f67fc62dcf308b14586a27880
Nacho Althabe <nacho.althabe@gmail.com>
2017-12-14 14:04:47 -0300

    Testing logs http://confluence.jetbrains.com/display/TCD7/Build+Script+Interaction+with+TeamCity

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-echo "Build Images"
+echo "##teamcity[<Build Images> status='starting']"
 docker-compose build
-echo "Push gateway"
+echo "##teamcity[<Build Images> status='end']"
+echo "##teamcity[<Push Image> image='himalaya_gateway' status='starting']"
 docker push meteoro.dxmarkets.com:5000/himalaya_gateway:latest
-echo "Push teleferic"
+echo "##teamcity[<Push Image> image='himalaya_gateway' status='end']"
+echo "##teamcity[<Push Image> image='himalaya_teleferic' status='starting']"
 docker push meteoro.dxmarkets.com:5000/himalaya_teleferic:latest
+echo "##teamcity[<Push Image> image='himalaya_teleferic' status='end']"
