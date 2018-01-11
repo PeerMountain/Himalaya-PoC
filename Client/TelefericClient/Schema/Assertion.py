09310c237d11cfbe2b78e54d1975de4b710f580f
Felipe Buiras <felipe.buiras@maecenas.co>
2018-01-11 15:21:53 -0300

    Add container handling to Assertion message class, keep working on hashing metadata

--- Client/TelefericClient/Schema/Assertion.py
+++ Client/TelefericClient/Schema/Assertion.py
@@ -3,50 +3,35 @@ import msgpack
 from collections import OrderedDict
 from Crypto.Hash import SHA256, HMAC
 
-from Cryptography import AES
+from Cryptography import AES, RSA
 from .MessageBody import MessageBody
 from .MessageEnvelope import MessageEnvelope
+from .MessageContent import MessageContent
 
 class Assertion(MessageEnvelope):
     subject_address = None
     assertions = None
 
     def __init__(self, subject_address, assertions, container_key=None):
-        assertions = frozenset(assertions)
         assertions = list(
             self.build_assertion_list(assertions, container_key)
-        ),
+        )
+        containers = list(
+            self.build_container_list(assertions)
+        )
+        metahashes = list(
+            self.build_meta_hash_list(assertions)
+        )
 
         message_body = MessageBody(
             subjectAddr=subject_address,
             assertions=assertions
         )
 
-        def build_meta_hashes(assertions):
-            for assertion in assertions:
-                for meta in assertion.metas:
-                    if meta.get('metaSalt'): continue
-                    salt = self.generate_random_bytes()
-                    pack = msgpack.pack(meta)
-                    salted_meta_hash = base64.b64encode(
-                        HMAC.new(
-                            salt, pack, "SHA256"
-                        ).digest()
-                    )
-
-                    meta.update({
-                        'metaSalt': salt,
-                    })
-
-                    yield meta, salted_meta_hash
-
-        metas = tuple(
-            x[0] for x in build_meta_hashes(assertions)
+        message_content = MessageContent(
+            message_type=0, # BodyTypes.Assertion.ANY,
+            message_body=message_body,
         )
-        meta_hashes = tuple(
-            x[1] for x in build_meta_hashes(assertions)
-        )
-
 
     def build_assertion_list(self, assertions, container_key):
         for assertion in assertions:
@@ -66,12 +51,55 @@ class Assertion(MessageEnvelope):
                 object_hash, self.client
             )
 
+            container_signature = self.identity.sign_message(
+                container_hash, self.client
+            )
+
             yield {
                 'validUntil': assertion.valid_until,
                 'retainUntil': assertion.retain_until,
                 'containerHash': container_hash,
                 'containerKey': container_key,
                 'objectHash': object_hash,
-                'objectSign': object_signature,
-                'metas': assertion.metas
+                'objectSign': object_signature, 
+                'metas': assertion.metas,
+                'container': container,
+                'containerSignature': container_signature,
+            }
+
+    def build_meta_hash_list(self, assertions):
+        for assertion in assertions:
+            metahashes = []
+            for meta in assertion.metas:
+                salt = self.generate_random_bytes()
+                pack = msgpack.pack(meta)
+                salted_meta_hash = base64.b64encode(
+                    HMAC.new(
+                        salt, pack, "SHA256"
+                    ).digest()
+                )
+
+                # FIXME I hope this updates the dicts living inside the assertions
+                meta.update({
+                    'metaSalt': salt,
+                })
+
+                metahashes.append(salted_meta_hash)
+            yield metahashes
+ 
+    def build_container_list(self, assertions, meta_hashes):
+        teleferic_pubkey = self.client.get_node_pubkey()
+        for i, assertion in enumerate(assertions):
+            yield {
+                'containerHash': assertion.get('containerHash'),
+                'objectHash': assertion.get('objectHash'),
+                'containerSig': assertion.pop('containerSignature'),
+                'metaHashes': meta_hashes[i],
+                'retainUntil': RSA(teleferic_pubkey).encrypt(
+                    assertion.get('retainUntil'), None
+                ),
+                'validUntil': RSA(teleferic_pubkey).encrypt(
+                    assertion.get('validUntil'), None
+                ),
+                'objectContainer': assertion.pop('container')
             }
