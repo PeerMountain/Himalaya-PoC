b157b56ec19e026e523f5b41410ced2a3eec135e
Felipe Buiras <felipe.buiras@maecenas.co>
2018-01-11 12:37:36 -0300

    [WIP] Assertion message envelope

--- Client/TelefericClient/Schema/Assertion.py
+++ Client/TelefericClient/Schema/Assertion.py
@@ -1,8 +1,77 @@
+import base64
+import msgpack
+from collections import OrderedDict
+from Crypto.Hash import SHA256, HMAC
+
+from Cryptography import AES
 from .MessageBody import MessageBody
+from .MessageEnvelope import MessageEnvelope
 
-class Assertion(MessageBody):
-  subject_address = None
-  assertions = None
+class Assertion(MessageEnvelope):
+    subject_address = None
+    assertions = None
 
-  def __init__(self, subject_address, assertions):
-    
+    def __init__(self, subject_address, assertions, container_key=None):
+        assertions = frozenset(assertions)
+        assertions = list(
+            self.build_assertion_list(assertions, container_key)
+        ),
+
+        message_body = MessageBody(
+            subjectAddr=subject_address,
+            assertions=assertions
+        )
+
+        def build_meta_hashes(assertions):
+            for assertion in assertions:
+                for meta in assertion.metas:
+                    if meta.get('metaSalt'): continue
+                    salt = self.generate_random_bytes()
+                    pack = msgpack.pack(meta)
+                    salted_meta_hash = base64.b64encode(
+                        HMAC.new(
+                            salt, pack, "SHA256"
+                        ).digest()
+                    )
+
+                    meta.update({
+                        'metaSalt': salt,
+                    })
+
+                    yield meta, salted_meta_hash
+
+        metas = tuple(
+            x[0] for x in build_meta_hashes(assertions)
+        )
+        meta_hashes = tuple(
+            x[1] for x in build_meta_hashes(assertions)
+        )
+
+
+    def build_assertion_list(self, assertions, container_key):
+        for assertion in assertions:
+            if not container_key:
+                container_key = self.generate_random_bytes()
+            cipher = AES(container_key)
+            container = cipher.encrypt(assertion.object)
+
+            object_hash = base64.b64encode(
+                SHA256.new(assertion.object).digest()
+            )
+            container_hash = base64.b64encode(
+                SHA256.new(container).digest()
+            )
+
+            object_signature = self.identity.sign_message(
+                object_hash, self.client
+            )
+
+            yield {
+                'validUntil': assertion.valid_until,
+                'retainUntil': assertion.retain_until,
+                'containerHash': container_hash,
+                'containerKey': container_key,
+                'objectHash': object_hash,
+                'objectSign': object_signature,
+                'metas': assertion.metas
+            }
