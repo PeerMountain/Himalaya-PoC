7f3c4d390cea179f583525446b472662c8a6d05b
Felipe Buiras <felipe.buiras@maecenas.co>
2018-01-12 14:58:27 -0300

    Modify test_assertio

--- Client/test_asertion.py
+++ Client/test_asertion.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
-from TelefericClient.Identity import Identity
 from collections import OrderedDict
+import datetime
+
+from TelefericClient.Identity import Identity
 from TelefericClient.Client import Client
 from TelefericClient.Schema.Assertion import Assertion
+
+now = datetime.datetime.now()
+tomorrow = now + datetime.timedelta(days=1)
 
 idn = Identity(open("/home/mori/dev_sk").read())
 readers = [idn]
 client = Client("http://192.168.252.14:8000/teleferic/")
-ass = [{'valid_until': 0, 'retain_until': 0, 'object': b'\x12\x34\x56\x78\x90', 'metas': [OrderedDict(**{'metaKey': 2, 'metaValue': 'Pepe Sarasa'})]}]
+ass = [
+    {
+        'valid_until': now.isoformat(),
+        'retain_until': tomorrow.isoformat(),
+        'object': b'\x12\x34\x56\x78\x90',
+        'metas': [
+            OrderedDict(**{
+                'metaKey': 2, 
+                'metaValue': 'Pepe Sarasa'
+            })
+        ]
+    }
+]
 
 assertion = Assertion(idn, client, ass, readers)
 # agarra la asercion recien creada 
