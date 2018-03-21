844df80fe2ed24e04c7591c815d10742aca39f2b
Ignacio Althabe <ignacio.althabe@dxmarkets.com>
2018-03-21 18:01:05 -0300

    Append test step

--- Teleferic/coverage.sh
+++ Teleferic/coverage.sh
@@ -1,11 +1,9 @@
 #/bin/bash
 
-coverage run --source='.'  manage.py test API --behave_format behave_teamcity:TeamcityFormatter
+coverage run --source='.'  manage.py test API
 
 coverage html
 
 mkdir -p /code/coverage
 
-zip -r /code/coverage/coverage.zip htmlcov/ -j htmlcov -m
-
-rm -R htmlcov
+mv htmlcov /code/coverage
