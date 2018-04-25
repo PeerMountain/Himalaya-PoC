fb54a4f6c4dc6b1a8da042e7d9fdf7904d0f616d
Jonatas Baldin <jonatas.baldin@gmail.com>
2018-04-25 21:23:58 +0530

    Remove AESOld class and make string checking on AES cryptography lib

--- Client/TelefericClient/Cryptography/AES.py
+++ Client/TelefericClient/Cryptography/AES.py
@@ -31,6 +31,10 @@ class AES:
             else bytes(random.randint(1, 255) for _ in range(16))
         )
         self.key = key
+
+        if isinstance(key, str):
+            key = key.encode()
+
         self.cipher = Base_AES.new(key, Base_AES.MODE_GCM, nonce=self.nonce)
 
     def decrypt(self, ciphertext: bytes, tag: Union[bytes, None]=None) -> bytes:
@@ -70,42 +74,3 @@ class AES:
         """
         ciphertext, _ = self.cipher.encrypt_and_digest(data)
         return base64.b64encode(ciphertext)
-
-
-# TODO: replace with correct AES encryption scheme from pycryptodome
-class AESOld():
-    """AES
-
-    AES algorithm implementation.
-    """
-
-    BLOCK_SIZE = 16
-    KEY_SIZE = 32
-    MODE = Base_AES.MODE_ECB
-
-    def __init__(self, key):
-        key_length = len(key)
-        if key_length > self.KEY_SIZE:
-            raise Exception(
-                'Key length must be lower or equal to {0}.'.format(self.KEY_SIZE))
-        elif key_length < self.KEY_SIZE:
-            self.key = self.__pad(key,self.KEY_SIZE)
-        else:
-            self.key = key
-        self.cipher = Base_AES.new(self.key, self.MODE)
-
-    def __pad(self, s, l=BLOCK_SIZE):
-        return s + (l - len(s) % l) * chr(l - len(s) % l).encode()
-
-    def __unpad(self, s):
-        return s[:-ord(s[len(s) - 1:])]
-
-    def encrypt(self, content):
-        local_content = self.__pad(content)
-        ciphed_content = self.cipher.encrypt(local_content)
-        return base64.b64encode(ciphed_content)
-
-    def decrypt(self, b64_ciphed_content):
-        ciphed_content = base64.b64decode(b64_ciphed_content)
-        content = self.cipher.decrypt(ciphed_content)
-        return self.__unpad(content)
